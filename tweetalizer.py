ccb8c32d32d367b65c0e470cdc7222fed9d4dd4a
efishe12 <efishe12@emich.edu>
2015-02-08 15:55:16 -0500

    Updated varible names

--- tweetalizer.py
+++ tweetalizer.py
@@ -8,32 +8,34 @@
 
 import sys
 
+
 def tweetalize():
     print("Welcome to the awesometastic tweetalizer! Start analyzing your tweets now!")
 
     while True:
-      tw = raw_input("What tweet would you like to analyze? To quit, just enter 'q'. ") #if using python 3, use input function
+        maxLength = 140
+        tweet = raw_input("What tweet would you like to analyze? To quit, just enter 'q'. ") #if using python 3, use input function
 
-      if tw == 'q' or tw == 'Q':
-          exit()
+        if tweet == 'q' or tweet == 'Q':
+            exit()
 
-      print("Tweet is " + str(len(tw)) + " characters long")
-      if len(tw) > 140:
-          print("The number of characters in this tweet is too damn high!\nThe limit is 140.")
+        print("Tweet is " + str(len(tweet)) + " characters long")
+        if len(tweet) > maxLength:
+            print("The number of characters in this tweet is too damn high!\nThe limit is 140.")
 
-      a = 0
-      for x in tw.split(' '):
-          if x[0] == "#":
-              print("Tweet contains the hashtag: " + x)
-              a += 1
-      print("The tweet has " + str(a) + " total #hashtags")
+        hashtag = 0
+        for words in tweet.split(' '):
+            if words[0] == "#":
+                print("Tweet contains the hashtag: " + words)
+                hashtag += 1
+        print("The tweet has " + str(hashtag) + " total #hashtags")
 
-      b = 0
-      for x in tw.split(' '):
-          if x[0] == "@":
-              print("Tweet contains the mention: " + x)
-              b += 1
-      print("The tweet has " + str(b) + " total @mentions")
+        mention = 0
+        for words in tweet.split(' '):
+            if words[0] == "@":
+                print("Tweet contains the mention: " + words)
+                mention += 1
+        print("The tweet has " + str(mention) + " total @mentions")
 
 
 tweetalize()
